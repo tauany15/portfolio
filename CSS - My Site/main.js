66c45c2a133d207466e759029356055fb83ca87d
tauany15 <tauany.santoss@hotmail.com>
2022-01-11 17:29:30 -0500

    Created Tech Skills button and Modal
    * Created the Tech Skills button
    * Created the Modal
    * Changed the Skills images sizes.
    * Added the js file

---
+++ CSS - My Site/main.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector("#modal-tech-skills");
+const modalBtn = document.querySelector("#myBtn");
+const span = document.querySelector("#close-btn");
+
+
+modalBtn.addEventListener("click", () =>{
+  modal.className = "open";
+});
+
+span.addEventListener("click", () =>{
+  modal.className = "close";
+});
