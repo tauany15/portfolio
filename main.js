b1459e57541f587a2965422774b6dd04045a12d2
tauany15 <tauany.santoss@hotmail.com>
2022-01-13 18:13:34 -0500

    Adjusts to the modal texts

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const modal = document.querySelector("#modal-tech-skills");
 const modalBtn = document.querySelector("#myBtn");
 const span = document.querySelector("#close-btn");
 
-
 modalBtn.addEventListener("click", () =>{
   modal.className = "open";
 });
